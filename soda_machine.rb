e123ce0e29a1d3875e28a5ac6d9dc697713a8dd4
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-10-11 10:13:10 -0700

    Pass current_inventory_count test

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,26 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.size 
   end
 
   def find_soda(soda_brand)
+    @sodas.first
+    if @sodas == soda_brand
+      @sodas
+    else
+      nil
+    end
   end
 
   def sell(soda_brand)
+    if @sodas.include?(soda_brand) == true
+      @cash << @sodas.price
+      @sodas.delete 
+      @sodas
+    else
+      nil
+    end
   end
 
 end
